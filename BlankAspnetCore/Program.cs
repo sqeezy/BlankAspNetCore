b1efa660e768259c30dafb66725a121b3f89f110
Anton Herzog <info@antonherzog.com>
2025-02-15 18:04:16 +0100

    New Stuff

--- BlankAspnetCore/Program.cs
+++ BlankAspnetCore/Program.cs
@@ -3,5 +3,6 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello updated again World!");
 app.MapGet("/whoami", () => "It'se me!");
+app.MapGet("/preview-feature", () => "This is super new!");
 
 app.Run();
