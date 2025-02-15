d25d2af2af96f3787cc79f74429eca045a7ebc08
Anton Herzog <info@antonherzog.com>
2025-02-15 17:55:36 +0100

    Random new api

--- BlankAspnetCore/Program.cs
+++ BlankAspnetCore/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello updated World!");
+app.MapGet("/", () => "Hello updated again World!");
+app.MapGet("/whoami", () => "It'se me!");
 
 app.Run();
