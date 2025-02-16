b2d47a2c967067ffa3dc91cef64973817719d414
Anton Herzog <info@antonherzog.com>
2025-02-16 15:08:34 +0100

    Add api with parameter

--- BlankAspnetCore/Program.cs
+++ BlankAspnetCore/Program.cs
@@ -7,5 +7,6 @@ app.MapHealthChecks("/healthz");
 app.MapGet("/", () => "Hello updated again World!");
 app.MapGet("/whoami", () => "It'se me!");
 app.MapGet("/preview-feature", () => "This is super new!");
+app.MapGet("/greet/{userId}", (int userId) => $"Hello {userId}!");
 
 app.Run();
